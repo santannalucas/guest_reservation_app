a554c04a9e6eb44781e1d29bd75f78ec91a96718
santannalucas <santanna.lucas@gmail.com>
2023-03-25 14:53:39 +1100

    initial commit

---
+++ db/migrate/20230325035309_create_reservations.rb
@@ -0,0 +1,22 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.string :reservation_code
+      t.date :start_date
+      t.date :end_date
+      t.integer :nights
+      t.integer :guests
+      t.integer :adults
+      t.integer :children
+      t.integer :infants
+      t.string :status
+      t.string :currency
+      t.decimal :payout_price
+      t.decimal :security_price
+      t.decimal :total_price
+      t.references :guest, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
